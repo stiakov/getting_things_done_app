07f17adbb539ac674f00e926886c8399586cf564
Santiago Torres G <thdealer@gmail.com>
2019-10-16 17:29:52 -0500

    package.json and webpack settings added

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  // mode: 'development',
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+            removeComments: false,
+            collapseWhitespace: false
+          }
+        }],
+      }
+    ],
+  },
+};
