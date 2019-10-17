5b0d2be411d9b31f3895ade52a76d99426338bef
Santiago Torres G <thdealer@gmail.com>
2019-10-17 17:33:17 -0500

    laying out cards

--- src/js/env.js
+++ src/js/env.js
@@ -1,27 +1,55 @@
 import fav from '../img/favicon.ico'
 import {compareAsc, format} from 'date-fns'
 
+const tag = {
+  getMainContainer: () => {
+    return document.getElementById('main-container');
+  },
+
+  getColContainer: () => {
+    return document.createElement('div');
+  },
+};
+
+const createColumn = () => {
+  const counter = 1;
+  const col = Object.assign(document.createElement('div'), { className: 'column' }, { id: 'col' + ( counter + 1) });
+  tag.getColContainer().appendChild(col);
+};
+
 const setup = {
   setFavicon: (() => {
     const setFav = document.getElementById('favicon');
     setFav.href = fav;
   })(),
+
   getDateNow: (() => {
     const date = format(new Date(), 'yyyy-MM-dd');
     return date.split('-');
   })(),
-  getMainContainer: () => {
-    return document.getElementById('main-container');
-  },
+
   setLayout: (() => {
     const colContainer = document.createElement('div');
-    colContainer.className = 'ui three column grid container';
-    for (let i = 0; i < 3; i += 1) {
-      const col = Object.assign(document.createElement('div'), {className: 'column'}, {id: 'col' + (i + 1) });
-      colContainer.appendChild(col);
-    }
-    document.getElementById('main-container').appendChild(colContainer);
+    colContainer.className = 'ui three columns grid container';
+    createColumn();
   })(),
+};
+
+const cards = {
+   layoutCardColumns: (() => {
+     const cols = document.getElementsByClassName('column');
+       console.log(cols);
+     for (const item in cols) {
+       const card = Object.assign(document.createElement('div'), { className: 'ui card' });
+       const content = Object.assign(document.createElement('div'), { className: 'content' });
+       const header = Object.assign(document.createElement('div'), { className: 'header' }, { content: 'Task Example'});
+       card.appendChild(content);
+       content.appendChild(header);
+       tag.getColContainer().appendChild(card);
+     }
+
+   })(),
+
 };
 
 const env = {};
