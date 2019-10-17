dc026c9e8d9a5aa8983eda6103743f77621aaae6
Santiago Torres G <thdealer@gmail.com>
2019-10-17 10:50:53 -0500

    loading script in the head tag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import '../semantic/dist/semantic.js';
 import '../semantic/dist/semantic.css';
 import '../src/css/style.css';
+
+const a = document.getElementById('cont');
+a.innerText = 'MY DIV';
