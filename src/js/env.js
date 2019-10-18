c814edf6d3224cc9ea16cdeb9feb10a19d7aed32
Santiago Torres G <thdealer@gmail.com>
2019-10-18 12:07:30 -0500

    styling columns and cards

--- src/js/env.js
+++ src/js/env.js
@@ -4,62 +4,61 @@ import fav from '../img/favicon.ico';
 const tag = {
   getMainContainer: () => document.getElementById('main-container'),
 
-  getColContainer: () => document.createElement('div')
+  getColContainer: () => document.getElementById('colContainer')
 };
 
+let counter = 1;
 const createColumn = () => {
-  const counter = 1;
   const col = Object.assign(
     document.createElement('div'),
     { className: 'column' },
-    { id: `col${counter + 1}` }
+    { id: `col${counter}` }
   );
+  counter += 1;
   tag.getColContainer().appendChild(col);
+  cards.layoutCardColumns(col);
 };
 
 const setup = {
-  setFavicon: (() => {
+  setFavicon: () => {
     const setFav = document.getElementById('favicon');
     setFav.href = fav;
-  })(),
+  },
 
-  getDateNow: (() => {
+  getDateNow: () => {
     const date = format(new Date(), 'yyyy-MM-dd');
     return date.split('-');
-  })(),
+  },
 
-  setLayout: (() => {
+  setLayout: () => {
     const colContainer = document.createElement('div');
-    colContainer.className = 'ui three columns grid container';
-    const main = tag.getMainContainer();
-    main.appendChild(colContainer);
+    Object.assign(colContainer, { className: 'ui width centered grid' }, { id: 'colContainer' });
+    tag.getMainContainer().appendChild(colContainer);
     createColumn();
-  })()
+  }
 };
 
 const cards = {
-  layoutCardColumns: (() => {
-    const cols = document.getElementsByClassName('column');
-    createColumn();
-    for (const item in cols) {
-      const card = Object.assign(document.createElement('div'), {
-        className: 'ui card'
-      });
-      const content = Object.assign(document.createElement('div'), {
-        className: 'content'
-      });
-      const header = Object.assign(
-        document.createElement('div'),
-        { className: 'header' },
-        { content: 'Task Example' }
-      );
-      card.appendChild(content);
-      content.appendChild(header);
-      const a = tag.getMainContainer();
-      a.appendChild(card);
-    }
-  })()
+  layoutCardColumns: (column) => {
+
+    const card = Object.assign(document.createElement('div'),  { className: 'ui card' });
+    const content = Object.assign(document.createElement('div'), { className: 'content' });
+    const header = Object.assign(
+      document.createElement('div'),
+      { className: 'header' },
+      { innerText: 'Task Example' }
+    );
+    card.appendChild(content);
+    content.appendChild(header);
+    column.appendChild(card);
+  }
 };
+const init = () => {
+  setup.setFavicon();
+  setup.setLayout();
+};
+
+init();
 
 const env = {};
 
