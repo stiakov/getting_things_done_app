b97cdab1c9e624fb144b652d539c7f46108cbc1c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-25 17:22:45 -0700

    updating styles, setting up imports and initial functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import '../semantic/dist/semantic';
 import '../semantic/dist/semantic.css';
 import './css/style.css';
-import setup from './js/setup';
+import setup from '../src/js/setup';
+import layout from '../src/js/layout';
 
 setup.setFavicon();
+setup.setColumnInit(setup.setProjectInit());
