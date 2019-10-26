d4576580e23fd193f6a979bd056fb19ff726f2b8
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-25 17:22:12 -0700

    adding basic logic for managing projects/tasks

---
+++ src/js/logic.js
@@ -0,0 +1,20 @@
+const systemManager = {
+  todoItem: (id, title, description, dueDate, priority = 0, status = false) => {
+    return {
+      id,
+      title,
+      description,
+      dueDate,
+      priority,
+      status
+    };
+  },
+
+  ProjectManager: {
+    newProject: (id, name, tasks = []) => ({ id, name, tasks })
+    // addToProject: (project, item) => {
+    //   project.tasks[item.id] = item.todo;
+    // }
+  }
+};
+export default systemManager;
