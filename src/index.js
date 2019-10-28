ca5c426bb6bcda8cabade8af07bca46e918792d8
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-28 16:29:20 -0700

    calling methods

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import '../semantic/dist/semantic.css';
 import './css/style.css';
 import setup from '../src/js/setup';
 import layout from '../src/js/layout';
+import trigger from '../src/js/trigger';
 
 setup.setFavicon();
 setup.setColumnInit(setup.setProjectInit());
+trigger.setEdit();
+trigger.setDone();
+trigger.setDelete();
