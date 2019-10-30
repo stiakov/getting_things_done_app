63fe536c737fce07b6f5118092259aa868c6b599
Santiago Torres G <thdealer@gmail.com>
2019-10-30 10:19:27 -0500

    cleaning

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,10 @@
 import behaviour from "./behaviour";
 
-const crea = (type, attrib = []) => {
+export const getById = (id) => document.getElementById(id);
+
+export const append = (parent, child) => parent.appendChild(child);
+
+export const create = (type, attrib = []) => {
   const elem = document.createElement(type);
   if (attrib.length > 0) {
     attrib.forEach((item) => {
@@ -9,65 +13,66 @@ const crea = (type, attrib = []) => {
   }
   return elem
 };
+
 const layout = {
   column: (project) => {
-    const colContainer = document.getElementById('colContainer');
-    const projectContainer = crea('div', [
+    const colContainer = getById('colContainer');
+    const projectContainer = create('div', [
       { className: 'column project-container' },
       { id: `col-${project.id}` }
     ]);
-    const mainSegment = crea('div', [{ className: 'ui placeholder segments' }, { id: `segments-${project.id}` }]);
-
-    const headerSegment = crea('div', [{ className: 'ui center aligned segment' }]);
-    const headerContent = crea('h2', [
+    const mainSegment = create('div', [{ className: 'ui placeholder segments' }, { id: `segments-${project.id}` }]);
+    const headerSegment = create('div', [{ className: 'ui center aligned segment' }]);
+    const headerContent = create('h2', [
       { className: 'header edit' },
       { innerText: project.name },
       { contentEditable: 'true' }
     ]);
-
-    colContainer.appendChild(projectContainer);
-    projectContainer.appendChild(mainSegment);
-    mainSegment.appendChild(headerSegment);
-    headerSegment.appendChild(headerContent);
+    append(colContainer, projectContainer);
+    append(projectContainer, mainSegment);
+    append(mainSegment, headerSegment);
+    append(headerSegment, headerContent);
     return colContainer;
   },
+
   taskProjectButton: (project) => {
-    const mainSegmnt = document.getElementById(`segments-${project.id}`);
-    const projectAddTaskBtn = crea('div', [
+    const mainSegmnt = getById(`segments-${project.id}`);
+    const projectAddTaskBtn = create('div', [
       { className: 'ui attached center aligned button btn-style' },
       { id: `newTaskBtn-${project.id}` },
       { innerText: 'Add Task' }
     ]);
     projectAddTaskBtn.addEventListener('click', () => layout.loadModal('task', layout.formNewTask(project)));
-    mainSegmnt.appendChild(projectAddTaskBtn);
+    append(mainSegmnt, projectAddTaskBtn);
     return mainSegmnt;
   },
+
   cardFields: () => {
-    const basicSeg = crea('div', [{ className: 'ui segment left-text' }]);
-    const sgmDivider = crea('div', [{ className: 'ui clearing divider' }]);
-    const sgmCard = crea('span', [
+    const basicSeg = create('div', [{ className: 'ui segment left-text' }]);
+    const sgmDivider = create('div', [{ className: 'ui clearing divider' }]);
+    const sgmCard = create('span', [
       { className: 'task-title edit' },
       { contentEditable: 'true' }
     ]);
-    const contentDescription = crea('div', [
+    const contentDescription = create('div', [
       { className: 'content edit' },
       { contentEditable: 'true' }
     ]);
-    const contentDate = crea('div', [
+    const contentDate = create('div', [
       { className: 'meta edit' },
       { contentEditable: 'true' }
     ]);
-    const contentButtons = crea('div', [{ className: 'meta' }]);
-    const deleteBtn = crea('label', [
+    const contentButtons = create('div', [{ className: 'meta' }]);
+    const deleteBtn = create('label', [
       { className: 'ui right icon label delete' },
       { innerText: 'Delete' }
     ]);
-    const deleteIcon = crea('i', [{ className: 'trash alternate icon' }]);
-    const doneBtn = crea('label', [
+    const deleteIcon = create('i', [{ className: 'trash alternate icon' }]);
+    const doneBtn = create('label', [
       { className: 'ui right icon label done' },
       { innerText: 'Complete' }
     ]);
-    const doneIcon = crea('i', [{ className: 'check circle icon' }]);
+    const doneIcon = create('i', [{ className: 'check circle icon' }]);
 
     return { segment: basicSeg,
       divider: sgmDivider,
@@ -82,7 +87,7 @@ const layout = {
     }
   },
   task: (project) => {
-    const mainSegmnt = document.getElementById(`segments-${project.id}`);
+    const mainSegmnt = getById(`segments-${project.id}`);
     const card = layout.cardFields();
     card['title'].innerText = project.tasks[0].title;
     card['content'].innerText = project.tasks[0].description;
@@ -139,34 +144,34 @@ const layout = {
       }
     ];
 
-    const formContainer = crea('div', [{ id: 'card-fields' }]);
+    const formContainer = create('div', [{ id: 'card-fields' }]);
 
     fields.forEach((input) => {
-      const fieldCont = crea('div', [{ className: 'field' }]);
-      const nameField = crea('input',[
+      const fieldCont = create('div', [{ className: 'field' }]);
+      const nameField = create('input',[
         { type: input.type },
         { placeholder: input.placeholder },
         { name: input.name },
         { id: input.id }
       ]);
-      fieldCont.appendChild(nameField);
-      formContainer.appendChild(fieldCont);
+      append(fieldCont, nameField);
+      append(formContainer, fieldCont);
     });
 
     checkboxes.forEach((input) => {
-      const checkCont = crea('div', [{ className: 'field' }]);
-      const checkField = crea('div', [{ className: 'ui checkbox' }]);
-      const boxField = crea('input', [
+      const checkCont = create('div', [{ className: 'field' }]);
+      const checkField = create('div', [{ className: 'ui checkbox' }]);
+      const boxField = create('input', [
         { type: input.type },
         { id: input.id }
       ]);
-      const checkLabel = crea('label', [{ innerText: input.name }]);
-      checkCont.appendChild(checkField);
-      checkField.appendChild(boxField);
-      checkField.appendChild(checkLabel);
-      formContainer.appendChild(checkCont);
+      const checkLabel = create('label', [{ innerText: input.name }]);
+      append(checkCont, checkField);
+      append(checkField, boxField);
+      append(checkField, checkLabel);
+      append(formContainer, checkCont);
     });
-    const addTaskBtn = crea('button', [
+    const addTaskBtn = create('button', [
       { className: 'ui button btn-style extra-space' },
       { type: 'submit' },
       { id: `create-task-${project.id}` },
@@ -175,21 +180,21 @@ const layout = {
 
     addTaskBtn.addEventListener('click', () => {
       behaviour.addTaskToProject(project);
-      const main = document.getElementById('main-container');
-      const modal = document.getElementById('modal');
+      const main = getById('main-container');
+      const modal = getById('modal');
       main.removeChild(modal);
     });
-    formContainer.appendChild(addTaskBtn);
 
+    append(formContainer, addTaskBtn);
     return formContainer;
   },
   formNewProject: () => {
-    const title = crea('input', [
+    const title = create('input', [
       { type: 'text' },
       { placeholder: 'Name your project' },
       { id: 'project-name' }
     ]);
-    const button = crea('button', [
+    const button = create('button', [
       { className: 'ui button btn-style extra-space' },
       { innerText: 'Create Project' }
     ]);
@@ -197,57 +202,59 @@ const layout = {
     return { title, button}
   },
   loadModal: (from, fields) => {
-    // from should be 'project' or 'task'
+    // from must be 'project' or 'task'
     const modalTitle = (from === 'project') ? 'New Project' : 'New Task';
     const modalLabel = (from === 'project') ? 'Start a new project' : 'Write a new task';
 
-    const mainContainer = document.getElementById('main-container');
-    const modal = crea('div', [
+    const mainContainer = getById('main-container');
+    let modal = create('div', [
       { className: 'ui dimmer modals page transition visible active underlay' },
       { tabIndex: 1 },
       { id: 'modal'}
     ]);
 
     document.body.addEventListener('keydown', (e) => {
-      if (e.key === 'Escape') mainContainer.removeChild(modal);
+      if (e.key === 'Escape') {
+        if (modal) mainContainer.removeChild(modal);
+        modal = undefined;
+      }
     });
 
-    const modalGrid = crea('div', [
+    const modalGrid = create('div', [
       { className: 'ui centered grid' },
       { style: 'margin-top:20vh' }
     ]);
-    const card = crea('div', [{ className: 'ui card extra-space overlay' }]);
-
-    const cardHeader = crea('div', [
+    const card = create('div', [{ className: 'ui card extra-space overlay' }]);
+    const cardHeader = create('div', [
       { className: 'ui medium header extra-space' },
       { innerText: modalTitle }
     ]);
-    const cardSub = crea('div', [
+    const cardSub = create('div', [
       { className: 'meta' },
       { innerText: modalLabel }
     ]);
-    const cardContent = crea('div', [{ className: 'content ui form extra-space' }]);
-
-    const cardFooter = crea('div', [
+    const cardContent = create('div', [{ className: 'content ui form extra-space' }]);
+    const cardFooter = create('div', [
       { className: 'meta' },
       { innerText: 'or press Escape to exit' }
     ]);
+
     if  (from === 'project') {
-      let cardFields = crea('div', [{ id: 'card-fields' }]);
-      cardFields.appendChild(fields['title']);
-      cardFields.appendChild(fields['button']);
-      cardContent.appendChild(cardFields);
+      let cardFields = create('div', [{ id: 'card-fields' }]);
+      append(cardFields, fields['title']);
+      append(cardFields, fields['button']);
+      append(cardContent, cardFields);
     } else {
-      cardContent.appendChild(fields);
+      append(cardContent, fields);
     }
 
-    mainContainer.appendChild(modal);
-    modal.appendChild(modalGrid);
-    modalGrid.appendChild(card);
-    card.appendChild(cardHeader);
-    card.appendChild(cardSub);
-    card.appendChild(cardContent);
-    card.appendChild(cardFooter);
+    append(mainContainer, modal);
+    append(modal, modalGrid);
+    append(modalGrid, card);
+    append(card, cardHeader);
+    append(card, cardSub);
+    append(card, cardContent);
+    append(card, cardFooter);
   }
 };
 export default layout;
