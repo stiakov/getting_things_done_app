b3a57548979b6515ab8c26db0c5db0814a72f7bc
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-30 14:28:35 -0700

    refactor behaviours

--- src/js/behaviour.js
+++ src/js/behaviour.js
@@ -1,15 +1,16 @@
 import layout from './layout';
 import setup from './setup';
 import systemManager from './logic';
+import { getById } from './layout';
 
 const sm = systemManager;
 
 const behaviour = {
   getTaskData: () => {
     let task_id = setup.getTaskCounter();
-    const task_title = document.getElementById('task-title').value;
-    const task_description = document.getElementById('task-description').value;
-    const task_date = document.getElementById('task-date').value;
+    const task_title = getById('task-title').value;
+    const task_description = getById('task-description').value;
+    const task_date = getById('task-date').value;
     const item = sm.todoItem(task_id, task_title, task_description, task_date);
 
     return item;
@@ -27,7 +28,7 @@ const behaviour = {
     let tempFilter = tempData.tasks.filter((task) => task.id !== item.id);
     tempData.tasks = tempFilter;
     localStorage.setItem(project.id, JSON.stringify(tempData));
-    const principal = document.getElementById(`sgc-${item.id}`);
+    const principal = getById(`sgc-${item.id}`);
     principal.parentElement.removeChild(principal);
   },
   editStatus: (project, item) => {
@@ -35,14 +36,14 @@ const behaviour = {
       if (task.id == item.id) {
         task.status = !task.status ? true : false;
         localStorage.setItem(project.id, JSON.stringify(project));
-        const card = document.getElementById(`sgc-${item.id}`);
+        const card = getById(`sgc-${item.id}`);
         card.classList.toggle('false');
       }
     });
   },
   getNewProjectData: () => {
     let project_id = setup.counterProj;
-    const project_name = document.getElementById('project-name').value;
+    const project_name = getById('project-name').value;
     const project = sm.newProject(project_id, project_name);
     return project;
   },
