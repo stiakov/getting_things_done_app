656e1a7d1a430bf474345db2aa458c1dc223b365
Santiago Torres G <thdealer@gmail.com>
2019-11-08 21:18:40 -0500

    updating transpiled files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import '../semantic/dist/semantic';
-import '../semantic/dist/semantic.css';
-import './css/style.css';
-import setup from '../src/js/setup';
-import layout from '../src/js/layout';
-
-setup.setFavicon();
-layout.addNewProjectBtn();
-setup.setProjectInit();
